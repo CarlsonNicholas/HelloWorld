2ac2e8d52b92272eafc468228aca8fe76524b56c
Nick Carlson <CarlsonNicholas82@gmail.com>
2022-11-17 12:42:51 -0500

    An unBEElievable inital commit!

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("According to all known laws\r\nof aviation,\r\n\r\n  \r\nthere is no way a bee\r\nshould be able to fly.\r\n\r\n  \r\nIts wings are too small to get\r\nits fat little body off the ground.\r\n\r\n  \r\nThe bee, of course, flies anyway because bees don't care nwhat humans think is impossible.");
